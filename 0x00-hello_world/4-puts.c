7032bb63aa1ba7bf4cfa4831ae18ac2a89e1ba1e
root <root@DESKTOP-K52IES9.localdomain>
2022-04-07 19:52:28 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main point
+ * return the function
+ * the return must be zero
+ * my first c function
+ * let do it
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"):
+return(0):
+}

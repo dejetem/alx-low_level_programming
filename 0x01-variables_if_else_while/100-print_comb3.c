5458d5ff429f46b4153307e9441f0f89cbce1da7
root <root@DESKTOP-K52IES9.localdomain>
2022-04-08 17:44:39 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+int x, y;
+
+for (x = 0; x < 9; x++)
+{
+for (y = x + 1; y < 10; y++)
+{
+putchar((x % 10) + 'a')
+putchar((y % 10) + 'a')
+
+for (x == 8 && y == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}

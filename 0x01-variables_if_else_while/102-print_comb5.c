cec899a74322a689f8d3c2019df9908ab4c4950e
root <root@DESKTOP-K52IES9.localdomain>
2022-04-08 19:14:00 +0100

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+int x, y;
+
+for (x = 0; x <= 98; x++)
+{
+for (y = x + 1; y <= 99; y++)
+{
+putchar((x / 10) + '0');
+putchar((x % 10) + '0');
+putchar( );
+putchar((y / 10) + '0');
+putchar((y % 10) + '0');
+
+if (x == 98 && y == 99)
+continue;
+
+putchar( );
+putchar( );
+}
+}
+
+putchar('\n');
+
+return (0);
+}

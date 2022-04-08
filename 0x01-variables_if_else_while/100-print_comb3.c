21b328fc036fad86c8a4e10ad901d8c1d1198b9e
root <root@DESKTOP-K52IES9.localdomain>
2022-04-08 17:45:56 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ for (x = 0; x < 9; x++)
 {
 for (y = x + 1; y < 10; y++)
 {
-putchar((x % 10) + 'a')
-putchar((y % 10) + 'a')
+putchar((x % 10) + 'a');
+putchar((y % 10) + 'a');
 
 for (x == 8 && y == 9)
 continue;

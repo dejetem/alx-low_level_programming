3994c87465ba9b322bbd7cb4e9c3517eb30c74e1
root <root@DESKTOP-K52IES9.localdomain>
2022-04-08 03:32:51 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 if success
+ */
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\n" - Dora korpar, 2015-10-19"));
+return (1);
+}

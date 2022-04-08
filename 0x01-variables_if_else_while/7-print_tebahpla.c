b0e52660e960d3391611aa2d23a6678deb600b88
root <root@DESKTOP-K52IES9.localdomain>
2022-04-08 17:22:08 +0100

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+char i;
+
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}

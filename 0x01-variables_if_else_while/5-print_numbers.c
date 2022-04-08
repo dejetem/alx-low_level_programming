1579b32c2a2a021b77147e55a997d6471c186e84
root <root@DESKTOP-K52IES9.localdomain>
2022-04-08 17:06:08 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+char i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf('\n');
+
+return (0);
+}

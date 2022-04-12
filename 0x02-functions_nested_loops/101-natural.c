4d5c80e8bcbfb5daddb30a59b158819ad1c26501
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 18:36:43 +0100

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: nothing if success
+ */
+int main(void)
+{
+int a, sum = 0;
+
+for (a = 0; a < 1024; a++)
+{
+if ((a % 3) == 0 || (a % 5) == 0)
+sum += a;
+}
+printf("%d\n", sum);
+
+return (0);
+}

c79760804dc9f77e2d090a8d25343451ebb5df39
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 16:49:14 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,27 +6,27 @@
  */
 void times_table(void)
 {
-int a;
-int b;
-int c;
+int x;
+int y;
+int z;
 
-for (a = 0; a <= 9; a++)
+for (x = 0; x <= 9; x++)
 {
-for (b = 0; b <= 9; b++)
+for (y = 0; y <= 9; y++)
 {
-c = a * b;
-if ((c / 10) == 0)
+z = x * y;
+if ((z / 10) == 0)
 {
-if (b == 0)
+if (y == 0)
 {
 _putchar ('0');
 }
-if (b != 0)
+if (y != 0)
 {
 _putchar (' ');
-_putchar ((c % 10) + '0');
+_putchar ((z % 10) + '0');
 }
-if (b < 9)
+if (y < 9)
 {
 _putchar(',');
 _putchar (' ');
@@ -34,9 +34,9 @@ _putchar (' ');
 }
 else
 {
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
+_putchar ((z / 10) + '0');
+_putchar ((z % 10) + '0');
+if (y < 9)
 {
 _putchar(',');
 _putchar (' ');

42ecbb00518794725f8ee9a438ad250f2aa8f2dc
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 15:18:57 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - absolute value of an integer
+ * @n: The int to print
+ * Return: Always 0
+ */
+int _abs(int r)
+{
+if (r < 0)
+{
+return (r * (-1));
+}
+else if (r == 0)
+{
+return (0);
+}
+else
+{
+return (r);
+}
+}

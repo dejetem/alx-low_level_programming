2f590ed2dd7a818f05f31ca5d1b162e6ba20ac6c
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 19:36:35 +0100

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - sum of even fibonacci number not exceed 4000000
+ *
+ * Return: nothing if success
+ */
+int main(void)
+{
+unsigned long a, b, c, d, e;
+
+b = e = 0;
+c = 1;
+for (a = 0; a < 50; a++)
+{
+d = b + c;
+b = c;
+c = d;
+if (d % 2 == 0; && d < 4000000)
+{
+e += d;
+}
+}
+printf("%lu\n", e);
+return (0);
+}

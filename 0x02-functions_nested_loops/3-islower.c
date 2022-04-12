6b86418a6cfebf709ba0e979c056ee5c8b9f9fbf
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 14:46:43 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: nothing if success
+ */
+int _islower(int c)
+{
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}

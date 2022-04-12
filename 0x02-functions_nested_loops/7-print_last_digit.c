ac443d58ff2133441fa6a91aaee3180ae583febc
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 15:28:42 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit
+ * @i: The int to print
+ * Return: Always 0
+ */
+int print_last_digit(int i)
+{
+int last_digit;
+if (i < 0)
+{
+last_digit = (-1 * (i % 10));
+_putchar (last_digit + '0');
+return (last_digit);
+}
+else
+{
+last_digit = (i % 10);
+_putchar (last_digit + '0');
+return (last_digit);
+}
+}

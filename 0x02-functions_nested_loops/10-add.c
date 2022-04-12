c57d9937b7da8e2a5f48efbc655f39cf664a25e1
root <root@DESKTOP-K52IES9.localdomain>
2022-04-12 17:25:37 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - add -prints add two numbers
+ * @a: print int a
+ * @b: print int b
+ * Return: nothing if success
+ */
+int add(int a, int b)
+{
+return (a + b);
+}

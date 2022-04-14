a471770a085cf2552acd6c559381ead75e5008f7
root <root@DESKTOP-K52IES9.localdomain>
2022-04-14 19:14:54 +0100

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * Auth: Simon Jonathan
+ */
+
+/**
+ * mul - Multiples two integers
+ * @a: The first integer to be multiplied
+ * @b: The second integer to be multiplied
+ * Return: The result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}

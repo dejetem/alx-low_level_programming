47c62a86f92f72af2cf6023e580c199ae351a80f
root <root@DESKTOP-K52IES9.localdomain>
2022-04-21 01:09:49 +0100

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * generates random valid passwords for the program 101-crackme
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}

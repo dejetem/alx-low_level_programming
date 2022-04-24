7ed97a184d4f4f153e8df0d423d13ef63390f4f1
root <root@DESKTOP-K52IES9.localdomain>
2022-04-24 14:53:07 +0100

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @n: string
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+int count = 0, i;
+int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+if (*(n + count) >= 97 && *(n + count) <= 122)
+*(n + count) = *(n + count) - 32;
+count++;
+while (*(n + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(n + count) == sep[i])
+{
+if ((*(n + (count + 1)) >= 97) && (*(n + (count + 1)) <= 122))
+*(n + (count + 1)) = *(n + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (n);
+}

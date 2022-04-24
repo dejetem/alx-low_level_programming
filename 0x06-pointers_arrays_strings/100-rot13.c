627fb3ad29e95085a7f9401223ccedac9a79bb21
root <root@DESKTOP-K52IES9.localdomain>
2022-04-24 15:05:13 +0100

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @n: string to code
+ * Return: the enconde string
+ */
+
+char *rot13(char *s)
+{
+int count = 0, i;
+char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+while (*(n + count) != '\0')
+{
+for (i = 0; i < 52; i++)
+{
+if (*(n + count) == alph[i])
+{
+*(n + count) = rot[i];
+break;
+}
+}
+count++;
+}
+return (n);
+}

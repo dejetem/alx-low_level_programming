d335a0521e7bf239061b98b11a1b5f2ed2d3afbd
root <root@DESKTOP-K52IES9.localdomain>
2022-04-26 22:38:11 +0100

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  set_string - set value of a pointer to a char
+ *  @s: value to be copied
+ *  @n: string
+ *  Return: void
+ */
+
+void set_string(char **s, char *n)
+{
+*s = n;
+}

00ab0f6caaaccf086fcf6ef507fc1baa08049c54
root <root@DESKTOP-K52IES9.localdomain>
2022-05-02 19:34:17 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: The size of the argv array
+ * @argv: An array of size argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}

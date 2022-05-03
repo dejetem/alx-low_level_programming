4560f905c53cc694c2a3d607561eb7d55fa18c5a
JONATHAN SIMON <52535603+dejetem@users.noreply.github.com>
2022-05-03 01:23:09 +0100

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,17 +3,18 @@
 
 /**
  * main - function
- * @argc: length of argv
- * @argv: number of argument
- * Return: Always 0
+ *@argc: length of argv
+ *@argv: number of argument
+ *Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
+/*Declaring variables*/
 int position, total, change, aux;
-int coins[] = {25, 10, 5, 2, 1}; 
+int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
-position = total = change = aux = 0;
+point = total = change = aux = 0;
 
 if (argc != 2)
 {
@@ -21,7 +22,7 @@ printf("Error\n");
 return (1);
 }
 
-total = atoi(argv[1]); 
+total = atoi(argv[1]); /*Covert str to int*/
 
 if (total <= 0)
 {
@@ -29,18 +30,19 @@ printf("0\n");
 return (0);
 }
 
+/*Declaring While*/
 
-while (coins[position] != '\0')
+while (coins[point] != '\0')
 
 {
-if (total >= coins[position])
+if (total >= coins[point])
 {
-aux = (total / coins[position]);
+aux = (total / coins[point]);
 change += aux;
-total -= coins[position] * aux;
+total -= coins[point] * aux;
 }
 
-position++;
+point++;
 
 }
 

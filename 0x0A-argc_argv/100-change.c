7d442da893b1a211356f0b7bf6af492a7fd4ad0c
root <root@DESKTOP-K52IES9.localdomain>
2022-05-03 01:18:40 +0100

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,20 +14,15 @@ int position, total, change, aux;
 int coins[] = {25, 10, 5, 2, 1}; 
 
 position = total = change = aux = 0;
-
 if (argc != 2)
 {
 printf("Error\n");
-
 return (1);
 }
-
 total = atoi(argv[1]); 
-
 if (total <= 0)
 {
 printf("0\n");
-
 return (0);
 }
 while (coins[position] != '\0')
@@ -41,6 +36,5 @@ total -= coins[position] * aux;
 position++;
 }
 printf("%d\n", change);
-
 return (0);
 }

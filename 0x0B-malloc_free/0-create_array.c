abafe85208a58b48697f93e9ab7ac236abcce5b7
root <root@DESKTOP-K52IES9.localdomain>
2022-05-05 16:21:27 +0100

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *create_array - array for prints a string
+ *@size: number elements array
+ *@c: char
+ *Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *p;
+unsigned int i = 0;
+
+if (size == 0)
+{
+return (NULL);
+}
+p = malloc(size * sizeof(char));
+if (p == NULL)
+{
+return (NULL);
+}
+while (i < size)
+}
+p[i] = c;
+i++;
+}
+return (p);
+}

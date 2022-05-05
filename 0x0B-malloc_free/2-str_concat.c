edac715f5c5b87e8a564b2feb498d90fc3cd84bb
root <root@DESKTOP-K52IES9.localdomain>
2022-05-05 16:45:27 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@ int _strlen(char *s)
 unsigned int i;
 
 i = 0;
-while (s[i] != '\0') 
+while (s[i] != '\0')
 {
 i++;
 }
@@ -19,7 +19,7 @@ return (i);
 }
 
 /**
- *str_concat - pointer 
+ *str_concat - pointer
  *@n1: Array 1
  *@n2: Array 2
  *Return: Always an array dinamic
@@ -27,33 +27,33 @@ return (i);
 
 char *str_concat(char *n1, char *n2)
 {
-	char *k;
-	unsigned int i, j, size;
+char *k;
+unsigned int i, j, size;
 
 
-	if (n1 == NULL)
-		n1 = "";
+if (n1 == NULL)
+n1 = "";
 
-	if (n2 == NULL)
-		n2 = "";
-	size = (_strlen(n1) + _strlen(n2) + 1);
+if (n2 == NULL)
+n2 = "";
+size = (_strlen(n1) + _strlen(n2) + 1);
 
-	k = (char *) malloc(size * sizeof(char));
+k = (char *) malloc(size * sizeof(char));
 
 
-	if (k == 0)
-	{
-		return (NULL);
-	}
+if (k == 0)
+{
+return (NULL);
+}
 
-	for (i = 0; *(n1 + i) != '\0'; i++)
-		*(k + i) = *(n1 + i);
+for (i = 0; *(n1 + i) != '\0'; i++)
+*(k + i) = *(n1 + i);
 
-	for (j = 0; *(n2 + j) != '\0'; j++)
-	{
-		*(k + i) = *(n2 + j);
-		i++;
-	}
+for (j = 0; *(n2 + j) != '\0'; j++)
+{
+*(k + i) = *(n2 + j);
+i++;
+}
 
-	return (k);
+return (k);
 }

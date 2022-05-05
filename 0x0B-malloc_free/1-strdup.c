8c9faa7c5dc1dea16b74e600a4fddd4d0a548e3b
root <root@DESKTOP-K52IES9.localdomain>
2022-05-05 16:33:10 +0100

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strlen - count array
+ *@s: array of elements
+ *Return: pointer,  NULL if it fails
+ */
+
+char *_strdup(char *s)
+{
+char *copy;
+unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+if (s == NULL)
+return (NULL);
+while (s[len])
+len++;
+copy = malloc(sizeof(char) * (len + 1));
+if (copy == NULL)
+return (NULL);
+while ((copy[i] = s[i]) != '\0')
+i++;
+return (copy);
+}

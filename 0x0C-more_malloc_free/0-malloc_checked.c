03537fa817d8d0269385910a6267f5f2c844fa6e
root <root@DESKTOP-K52IES9.localdomain>
2022-05-09 18:15:57 +0100

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *malloc_checked - array for prints a string
- *@b: number of memory
- *Return: void
+ * malloc_checked - array for prints a string
+ * @b: number of memory
+ * Return: void
  */
+
 
 void *malloc_checked(unsigned int b)
 {

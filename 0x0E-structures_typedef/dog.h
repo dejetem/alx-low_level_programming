b412727e34ae4dbdcc6e13ae4205a183fe2851ee
root <root@DESKTOP-K52IES9.localdomain>
2022-05-11 19:07:38 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,9 +11,9 @@
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 typedef struct dog dog_t;
@@ -22,4 +22,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif

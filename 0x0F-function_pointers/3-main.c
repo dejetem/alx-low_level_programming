4dbe29bf98558dba4dd5abc5fdaa305616541683
JONATHAN SIMON <52535603+dejetem@users.noreply.github.com>
2022-05-13 17:30:30 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,36 +9,38 @@
  */
 int main(int argc, char *argv[])
 {
-int arg1, arg2, result;
-char o;
-int (*func)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
-if (argc != 4)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-arg1 = atoi(argv[1]);
-arg2 = atoi(argv[3]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-func = get_op_func(argv[2]);
-if (!func)
-{
-printf("Error\n");
-exit(99);
-}
+	func = get_op_func(argv[2]);
 
-o = *argv[2];
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-if ((o == '/' || o == '%') && arg2 == 0)
-{
-printf("Error\n");
-exit(100);
-}
-result = func(arg1, arg2);
+	o = *argv[2];
 
-printf("%d\n", result);
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-return (0);
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
 }

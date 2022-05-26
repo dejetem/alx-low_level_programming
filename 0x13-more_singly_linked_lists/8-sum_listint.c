9b38f90ae0c8471718f486a10f650dfcc7f6693c
root <root@DESKTOP-K52IES9.localdomain>
2022-05-26 16:52:51 +0100

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,7 +3,7 @@ i#include "lists.h"
 /**
  * sum_listint - calculates the sum of all the data in a listint_t list
  *
- * @head: Pointer
+ * @head: Pointer to the first element of a linked list.
  * Return: sum of data
  */
 int sum_listint(listint_t *head)

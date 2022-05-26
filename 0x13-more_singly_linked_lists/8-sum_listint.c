62d76e4000e623c60919d3828d23a10e46407c6f
root <root@DESKTOP-K52IES9.localdomain>
2022-05-26 16:51:35 +0100

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+i#include "lists.h"
+
+/**
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ *
+ * @head: Pointer
+ * Return: sum of data
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+if (head == NULL)
+return (0);
+
+while (head != NULL)
+{
+sum = sum + head->n;
+head = head->next;
+}
+return (sum);
+}

3fddd9da78882dd7054b14b0e359c1c9c1a629a4
root <root@DESKTOP-K52IES9.localdomain>
2022-05-26 16:33:17 +0100

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer
+ */
+
+void free_listint(listint_t *head)
+{
+if (!head)
+{
+return;
+}
+if (head->next)
+{
+free_listint(head->next);
+}
+free(head);
+}
